13143cae249f69e86dfd542a320a53416a4f29bc
ATUL TINGRE <atultingre.work@gmail.com>
2023-05-19 03:43:15 +0530

    first commit

--- index.js
+++ index.js
@@ -28,9 +28,18 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/uploads", express.static(__dirname + "/uploads"));
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", `${BASE_URL}`);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 // REGISTER
 app.post("/register", async (req, res) => {
+
   const { username, password } = req.body;
   try {
     const userDoc = await User.create({
@@ -46,6 +55,7 @@ app.post("/register", async (req, res) => {
 
 // LOGIN
 app.post("/login", async (req, res) => {
+
   const { username, password } = req.body;
   const userDoc = await User.findOne({ username });
   const passOk = bcrypt.compareSync(password, userDoc.password);
@@ -65,6 +75,7 @@ app.post("/login", async (req, res) => {
 
 // PROFILE
 app.get("/profile", (req, res) => {
+
   const { token } = req.cookies;
   jwt.verify(token, secret, {}, (err, info) => {
     if (err) throw err;
@@ -74,11 +85,13 @@ app.get("/profile", (req, res) => {
 
 // LOGOUT
 app.post("/logout", (req, res) => {
+
   res.cookie("token", "").json("ok");
 });
 
 // CREATE POST
 app.post("/post", uploadMiddleware.single("file"), async (req, res) => {
+
   // FILE UPLOAD
   const { originalname, path } = req.file;
   const parts = originalname.split(".");
@@ -180,6 +193,7 @@ app.get("/post", async (req, res) => {
 
 // GET SINGLE POST
 app.get("/post/:id", async (req, res) => {
+  
   const { id } = req.params;
   const postDoc = await Post.findById(id).populate("author", ["username"]);
   res.json(postDoc);
